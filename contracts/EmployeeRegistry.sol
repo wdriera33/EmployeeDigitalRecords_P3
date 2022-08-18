701c727c646ca1efaece8864eca1c3f22acce9d3
wdriera33 <100449610+wdriera33@users.noreply.github.com>
2022-08-18 18:54:37 -0400

    updated sol file & json

--- contracts/EmployeeRegistry.sol
+++ contracts/EmployeeRegistry.sol
@@ -6,28 +6,29 @@ contract EmployeeRegistry is ERC721Full {
     constructor() public ERC721Full("EmployeeRegistryToken", "ERT") {}
 
     struct Employee {
+        address owner;
         string name;
         string birthYear;
         string originalCdlYear;
         string violations;
         string accidents;
         uint256 employeeScore;
-        string ertJson;
+        string attachment;
     }
 
     mapping(uint256 => Employee) public employeeData;
 
-    event Score(uint256 tokenId, uint256 employeeScore, string violations, string accidents, string reportURI, string ertJson);
+    event Score(uint256 tokenId, uint256 employeeScore, string violations, string accidents, string reportURI, string attachment);
     
     function imageUri(
         uint256 tokenId
 
     ) public view returns (string memory imageJson){
-        return employeeData[tokenId].ertJson;
+        return employeeData[tokenId].attachment;
     }
 
 
-    function onbordingEmployee(
+    function onboardingEmployee(
         address owner,
         string memory name,
         string memory birthYear,
@@ -35,15 +36,15 @@ contract EmployeeRegistry is ERC721Full {
         string memory violations,
         string memory accidents,
         uint256 initialEmployeeScore,
-        string memory tokenURI,
-        string memory ertJSON
+        string memory attachment
+       
     ) public returns (uint256) {
         uint256 tokenId = totalSupply();
 
         _mint(owner, tokenId);
-        _setTokenURI(tokenId, tokenURI);
+        _setTokenURI(tokenId, attachment);
 
-        employeeData[tokenId] = Employee(name, birthYear, originalCdlYear, violations, accidents, initialEmployeeScore, ertJSON);
+        employeeData[tokenId] = Employee(owner, name, birthYear, originalCdlYear, violations, accidents, initialEmployeeScore, attachment);
 
         return tokenId;
     }
@@ -54,14 +55,14 @@ contract EmployeeRegistry is ERC721Full {
         string memory newAccidents,
         string memory newViolations,
         string memory reportURI,
-        string memory ertJSON
+        string memory attachment
         
     ) public returns (uint256) {
         employeeData[tokenId].employeeScore = newEmployeeScore;
         employeeData[tokenId].accidents = newAccidents;
         employeeData[tokenId].violations = newViolations;
         
-        emit Score(tokenId, newEmployeeScore, newViolations, newAccidents, reportURI, ertJSON);
+        emit Score(tokenId, newEmployeeScore, newViolations, newAccidents, reportURI, attachment);
 
         return (employeeData[tokenId].employeeScore);
     }
